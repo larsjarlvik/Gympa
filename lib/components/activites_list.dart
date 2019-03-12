4b54f713c2ed4105c43fa34b91c602aab2bd6270
larsjarlvik <lars.jarlvik@gmail.com>
2019-03-12 21:42:44 +0700

    Show week divider in day view

---
+++ lib/components/activites_list.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:gympa/models/activities.dart';
+import 'package:gympa/theme.dart';
+import 'package:intl/intl.dart';
+
+class ActivitiesList extends StatelessWidget {
+  final List<Activities> groupedActivities;
+  final format = new DateFormat("yyyy-MM-dd");
+
+  ActivitiesList(List<Activities> groupedActivities) : groupedActivities = groupedActivities;
+  
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      separatorBuilder: (context, index) => _getSeparator(index),
+      itemCount: groupedActivities.length,
+      itemBuilder: (context, index) {
+        final Activities ca = groupedActivities[index];
+
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.fromLTRB(15.0, 6.0, 15.0, 6.0),
+              child: Text(format.format(ca.date), style: TextStyles.body(context)),
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(15.0, 0.0, 15.0, 10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  _buildIcon(context, 'gym', ca.gym),
+                  _buildIcon(context, 'sport', ca.sport),
+                  _buildIcon(context, 'running', ca.running),
+                  _buildIcon(context, 'walking', ca.walking),
+                ],
+              ),
+            ),
+          ]
+        );
+      },
+    );
+  }
+
+  _getSeparator(int index) {
+    return index > 0 && groupedActivities[index - 1].date.weekday < groupedActivities[index].date.weekday ? 
+      Divider(
+        color: Colors.tealAccent,
+        height: 20.0,
+      ) : 
+      Divider(
+        color: Colors.white10,
+        height: 5.0,
+      );
+  }
+
+  _buildIcon(BuildContext context, String icon, int minutes) {
+    return Expanded(
+      flex: 1,
+      child: Opacity(
+        opacity: minutes > 0 ? 1.0 : 0.2,
+        child: Row(
+          children: [
+            Padding(
+              padding: EdgeInsets.fromLTRB(0.0, 0.0, 8.0, 0.0),
+              child: Image(image: AssetImage('assets/$icon.png'), height: 20.0)
+            ),
+            Opacity(
+              child: Text('${minutes.toString()} min', style: TextStyles.body(context)),
+              opacity: 0.7,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/activity_list_page.dart
+++ lib/pages/activity_list_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gympa/components/activites_list.dart';
 import 'package:gympa/components/spinner.dart';
 import 'package:gympa/components/page_container.dart';
 import 'package:gympa/models/activities.dart';
@@ -27,8 +28,6 @@ class _ActivityListPage extends State<ActivityListPage> with RouteAware  {
   var activeGrouping = Groupings.Daily;
   var groupedActivities = new List<Activities>();
   var loading = false;
-
-  final format = new DateFormat("yyyy-MM-dd");
 
   final RouteObserver routeObserver;
   
@@ -80,7 +79,7 @@ class _ActivityListPage extends State<ActivityListPage> with RouteAware  {
           _buildGroupPills(),
           Spinner(loading),
           new Expanded(
-            child: _buildActivitiesList(),
+            child: ActivitiesList(groupedActivities),
           ),
         ],
       ),
@@ -113,62 +112,6 @@ class _ActivityListPage extends State<ActivityListPage> with RouteAware  {
           onPressed: () => callback(),
         ),
       )
-    );
-  }
-
-  _buildActivitiesList() {
-    return ListView.separated(
-      separatorBuilder: (context, index) => Divider(
-        color: Colors.white10,
-        height: 5.0,
-      ),
-      itemCount: groupedActivities.length,
-      itemBuilder: (context, index) {
-        final Activities ca = groupedActivities[index];
-
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: EdgeInsets.fromLTRB(15.0, 8.0, 15.0, 6.0),
-              child: Text(format.format(ca.date), style: TextStyles.body(context)),
-            ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(15.0, 0.0, 15.0, 10.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  _buildIcon('gym', ca.gym),
-                  _buildIcon('sport', ca.sport),
-                  _buildIcon('running', ca.running),
-                  _buildIcon('walking', ca.walking),
-                ],
-              ),
-            ),
-          ]
-        );
-      },
-    );
-  }
-
-  _buildIcon(String icon, int minutes) {
-    return Expanded(
-      flex: 1,
-      child: Opacity(
-        opacity: minutes > 0 ? 1.0 : 0.2,
-        child: Row(
-          children: [
-            Padding(
-              padding: EdgeInsets.fromLTRB(0.0, 0.0, 8.0, 0.0),
-              child: Image(image: AssetImage('assets/$icon.png'), height: 20.0)
-            ),
-            Opacity(
-              child: Text('${minutes.toString()} min', style: TextStyles.body(context)),
-              opacity: 0.7,
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
